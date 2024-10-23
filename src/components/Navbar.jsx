0fe650bfa9acd724ff47065ce840efc5c7a5a466
Titus Njema <njema3@gmail.com>
2024-10-23 10:27:08 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import './style.css';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <ul>
+            <li>Home</li>
+            <li>Tasks</li>
+            <li>Help</li>
+        </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar;
